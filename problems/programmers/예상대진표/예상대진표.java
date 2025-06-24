c17e11d91b729df9729ee436b9a74f7a4d933338
jeonggu <dev.jeonggu@gmail.com>
2025-06-24 21:23:20 +0900

    feat: PGS 389479 예상대진표
    feat: PGS 389479 예상대진표

---
+++ problems/programmers/예상대진표/예상대진표.java
@@ -0,0 +1,19 @@
+// NOTE : 예상대진표
+// NOTE : 프로그래머스
+// NOTE : https://school.programmers.co.kr/learn/courses/30/lessons/389479?language=java
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 1;
+        while(true){
+            a = (a+1)/2;
+            b = (b+1)/2;
+            if(a == b) break;
+
+            answer++;
+        }   
+        return answer;
+    }
+}
